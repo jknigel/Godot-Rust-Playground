cac58941b26f45b36c6a6dddd2e1e08b6313778c
jknigel <ajknigel360@gmail.com>
2025-11-11 00:03:19 +0800

    Development 0.0.2

--- rust_library/src/lib.rs
+++ rust_library/src/lib.rs
@@ -1,1 +1,25 @@
-mod player;
+use godot::prelude::*;
+
+// Define the class first
+#[derive(GodotClass)]
+#[class(base=Node)]
+struct HelloWorld {
+    #[base]
+    base: Base<Node>,
+}
+
+#[godot_api]
+impl INode for HelloWorld {
+    fn init(base: Base<Node>) -> Self {
+        Self { base }
+    }
+    fn ready(&mut self) {
+        godot_print!("Hello from a correctly built and loaded Rust library!");
+    }
+}
+
+// Register the class at the end
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
