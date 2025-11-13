48b9308021122a521654fb3a89cf49c72cf889f3
Jknigel <ajknigel360@gmail.com>
2025-11-14 01:34:28 +0800

    Development 0.0.4

--- rust_library/src/player.rs
+++ rust_library/src/player.rs
@@ -1,6 +1,6 @@
-use godot::prelude::*;
-use godot::classes::Sprite2D;
 use godot::classes::ISprite2D;
+use godot::classes::Sprite2D;
+use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
@@ -8,14 +8,14 @@ pub struct Player {
     speed: f64,
     angular_speed: f64,
 
-    base: Base<Sprite2D>
+    base: Base<Sprite2D>,
 }
 
 #[godot_api]
 impl ISprite2D for Player {
     fn init(base: Base<Sprite2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
-        
+
         Self {
             speed: 400.0,
             angular_speed: std::f64::consts::PI,
@@ -24,12 +24,12 @@ impl ISprite2D for Player {
     }
 
     fn physics_process(&mut self, delta: f64) {
-        // In GDScript, this would be: 
+        // In GDScript, this would be:
         // rotation += angular_speed * delta
-    
+
         let radians = (self.angular_speed * delta) as f32;
         self.base_mut().rotate(radians);
-        // The 'rotate' method requires a f32, 
+        // The 'rotate' method requires a f32,
         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
 
         let rotation = self.base().get_rotation();
@@ -42,10 +42,14 @@ impl ISprite2D for Player {
 impl Player {
     #[func]
     fn increase_speed(&mut self, amount: f64) {
-        self.speed += amount;
-        self.signals().speed_increased().emit();
-    }
+        let old_speed = self.speed;
+        let new_speed = old_speed + amount;
 
-    #[signal]
-    fn speed_increased();
-}
+        let speed_ratio = new_speed / old_speed;
+
+        self.angular_speed *= speed_ratio;
+        self.speed = new_speed;
+    }
+        #[signal]
+        fn speed_increased();
+    }
